56743b510117320946dbbdbbcaffe306b62898bd
David Catalan <catalan@adobe.com>
2023-03-24 15:15:23 +0100

    fix: semantic release build

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -8,9 +8,6 @@ module.exports = {
     ["@semantic-release/npm", {
       npmPublish: false,
     }],
-    ['@semantic-release/exec', {
-      prepareCmd: 'npm run build',
-    }],
     ['@semantic-release/git', {
       'assets': ['package.json', 'package-lock.json', 'CHANGELOG.md'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
