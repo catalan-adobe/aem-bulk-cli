8030bd92d8d7ca52efeb5f0c03ad7797e5e91676
David Catalan <catalan@adobe.com>
2023-05-04 15:55:13 +0200

    feat: add import command to cache resources of a webpage locally

---
+++ cmds/importer/cache.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+// imports
+const path = require('path');
+const { cliWorkerHandler } = require('../../src/cliWorkerHandler');
+
+function yargsBuilder(yargs) {
+  return yargs
+    .option('interactive', {
+      alias: 'i',
+      describe: 'Start the application in interactive mode, you will be prompted to copy/paste the list of URLs directly in the terminal. Enter an empty line to finish the process',
+      type: 'boolean',
+    })
+    .option('file', {
+      alias: 'f',
+      describe: 'Path to a text file containing the list of URLs to deliver (urls pattern: "https://<branch>--<repo>--<owner>.hlx.page/<path>")',
+      type: 'string',
+    })
+    .conflicts('f', 'i')
+    .option('output-folder', {
+      alias: 'o',
+      describe: 'The target folder for the generated data',
+      type: 'string',
+      default: 'cache',
+    })
+    .option('workers', {
+      alias: 'w',
+      describe: 'Number of workers to use (max. 8)',
+      type: 'number',
+      default: 1,
+      coerce: (value) => {
+        if (value > 8) {
+          terminal.yellow('Warning: Maximum number of workers is 8. Using 8 workers instead.\n');
+          return 8;
+        }
+        return value;
+      },
+    })
+    .option('no-headless', {
+      describe: 'Starts the browser in non-headless mode. Useful for debugging. Also, it forces workers to 1.',
+      type: 'boolean',
+    });
+}
+
+/*
+ * Main
+ */
+
+exports.desc = 'Cache webpages resources for a list of URLs';
+exports.builder = yargsBuilder;
+exports.handler = async (argv) => {
+  // create output folder structure
+  const outputFolder = path.isAbsolute(argv.outputFolder)
+  ? argv.outputFolder
+  : path.join(process.cwd(), argv.outputFolder);
+  
+  // headless true unless --no-headless is passed
+  const headless = argv.headless !== undefined ? argv.headless : true;
+
+  // execute preparation of the sections mapping
+  return cliWorkerHandler('importer_cache_worker.js', {
+    outputFolder,
+    headless,
+  }, argv);
+};
---
+++ workers/importer_cache_worker.js
@@ -0,0 +1,55 @@
+const { parentPort } = require('worker_threads');
+const pUtils = require('path');
+const fs = require('fs');
+
+/*
+ * Worker thread
+ */
+
+// Listen for messages from the parent thread
+parentPort.on('message', async (msg) => {
+  if (msg && msg.type === 'exit') {
+    // If the parent thread sent 'exit', exit the worker thread
+    process.exit();
+  } else {
+    const importerLib = await import('franklin-bulk-shared');
+
+    try {
+      const u = new URL(msg.url);
+      const OUTPUT_FOLDER = pUtils.join(msg.options.outputFolder, u.hostname); // `${process.cwd()}/output`;
+      const [browser, page] = await importerLib.Puppeteer.initBrowser({ headless: msg.options.headless });
+      
+      await importerLib.Puppeteer.runStepsSequence(
+        page,
+        msg.url,
+        [
+          importerLib.Puppeteer.Steps.postLoadWait(1000),
+          importerLib.Puppeteer.Steps.GDPRAutoConsent(),
+          importerLib.Puppeteer.Steps.smartScroll(),
+          // importerLib.Puppeteer.Steps.execAsync(async (browserPage) => {
+          //   await browserPage.keyboard.press('Escape');
+          // }),
+          importerLib.Puppeteer.Steps.fullPageScreenshot({ outputFolder: OUTPUT_FOLDER }),
+          importerLib.Puppeteer.Steps.cacheResources({ outputFolder: OUTPUT_FOLDER }),
+        ],
+      );
+
+      // cool down
+      await importerLib.Time.sleep(250);
+
+      await browser.close();
+
+      parentPort.postMessage({
+        url: msg.url,
+        passed: true,
+        result: 'Success',
+      });
+    } catch (error) {
+      parentPort.postMessage({
+        url: msg.url,
+        passed: false,
+        result: error.message,
+      });
+    }
+  }
+});
