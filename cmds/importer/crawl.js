c5e265f0075312af0f2476c33003da5d99334ccf
David Catalan <catalan@adobe.com>
2023-06-01 15:35:40 +0200

    feat: add new importer command to crawl robots.txt + sitemaps

---
+++ cmds/importer/crawl.js
@@ -0,0 +1,191 @@
+#!/usr/bin/env node
+
+// imports
+const fs = require('fs');
+const path = require('path');
+const { terminal } = require('terminal-kit');
+
+/*
+ * CLI Command parameters
+ */
+
+function yargsBuilder(yargs) {
+  return yargs
+    .option('origin', {
+      alias: 'o',
+      describe: 'Origin URL to start crawling from',
+      demandOption: true,
+      type: 'string',
+    })
+    .option('simple-filter', {
+      alias: 's',
+      describe: 'Simple filter to apply to the URLs. Only URLs containing this string will be crawled',
+      type: 'string',
+    })
+    .option('output-file', {
+      alias: 'f',
+      describe: 'File where to save the list of discovered URLs',
+      default: 'urls.txt',
+      type: 'string',
+    })
+    .option('workers', {
+      alias: 'w',
+      describe: 'Number of workers to use (max. 8)',
+      type: 'number',
+      default: 1,
+      coerce: (value) => {
+        if (value > 5) {
+          terminal.yellow('Warning: Maximum number of workers is 5. Using 5 workers instead.\n');
+          return 5;
+        }
+        return value;
+      },
+    })
+    .option('timeout', {
+      alias: 't',
+      describe: 'HTTP Timeout in seconds',
+      type: 'number',
+      default: 10,
+    });
+}
+
+/*
+ * Helper function to handle sitemaps
+ */
+
+async function handleSitemaps(queue, sitemaps, options = {}) {
+  const fBulk = await import('franklin-bulk-shared');
+
+  if (options.simpleFilter) {
+    sitemaps = sitemaps.filter((sitemap) => sitemap.indexOf(options.simpleFilter) !== -1);
+  }
+
+  sitemaps.forEach((s) => console.log(`new sitemap to crawl ${s}`));
+
+  sitemaps.forEach(async (sitemap) => {
+    if (sitemap.indexOf('.rss') === -1) {
+      try {
+        await queue.add(async () => {
+          try {  
+            console.log('crawling sitemap', sitemap);
+            const s = await fBulk.Web.parseSitemapFromUrl(sitemap, { timeout: options.timeout });
+            if (s.sitemaps && s.sitemaps.length > 0) {
+              await handleSitemaps(queue, s.sitemaps.map((s) => s.url), options);
+            }
+            return s;
+          } catch (e) {
+            throw new Error(`parse sitemap (${sitemap}): ${e}`);
+          }
+        });
+      } catch (e) {
+        // nothing
+      }
+    }
+  });
+}
+
+/*
+ * Main
+ */
+
+exports.desc = 'Collect URLs from robots.txt and sitemaps';
+exports.builder = yargsBuilder;
+exports.handler = async (argv) => {
+  let urlsFileStream;
+
+  try {
+    const fBulk = await import('franklin-bulk-shared');
+    const PQueue = await import('p-queue');
+    
+    // init constants from CLI args
+    // crawl workers
+    const workers = argv.workers;
+    // validate origin URL
+    const originUrl = new URL(argv.origin).href;
+    // validate simple filter
+    const simpleFilter = argv.simpleFilter ? argv.simpleFilter : null;
+    // timeout
+    const timeout = argv.timeout * 1000;
+
+    // crawling constants
+    const sitemaps = [];
+    // array of errors discovered during crawling
+    // will be displayed at the end
+    const errors = [];
+    // crawling options
+    const crawlOptions = {
+      simpleFilter,
+      timeout,
+    };
+
+    // create stream to save the list of discovered URLs
+    const outputFile = path.isAbsolute(argv.outputFile)
+    ? argv.outputFile
+    : path.join(process.cwd(), argv.outputFile);
+    if (!(await fs.existsSync(path.dirname(outputFile)))) {
+      fs.mkdirSync(path.dirname(outputFile), { recursive: true });
+    }
+    urlsFileStream = fs.createWriteStream(outputFile);
+
+    // init first sitemaps list from origin URL
+    if (originUrl.endsWith('robots.txt')) {
+      const r = await fBulk.Web.parseRobotsTxt(originUrl, { timeout });
+      sitemaps.push(...r.getSitemaps());
+    } else if (originUrl.indexOf('sitemap') > -1) {
+      sitemaps.push(originUrl);
+    } else {
+      throw new Error(`unsupported origin URL (${originUrl}) it should point to a robots.txt or a sitemap`);
+    }
+ 
+    // init work queue
+    const queue = new PQueue.default({
+      concurrency: workers,
+      autoStart: false,
+    });
+
+    // triggered each time a job is completed
+    queue.on('completed', async (result) => {
+      if (result?.urls) {
+          console.log(`done parsing sitemap ${result.url}, found ${result.urls?.length || 0} url(s) and ${result.sitemaps?.length || 0} sitemap(s)`);
+          urlsFileStream.write(result.urls.map((u) => u.url).join('\n'));
+          urlsFileStream.write('\n');
+        }
+    });
+      
+    // concatenate errors and only display them at the end
+    queue.on('error', error => {
+      errors.push(error);
+    });
+
+    // crawl is done
+    // when the queue is idle, display result + errors
+    queue.on('idle', () => {
+      terminal.green(`Crawling done!\n`);
+      if (errors.length > 0) {
+        console.log('errors:', errors.map((e) => e.message));
+      }
+    });
+
+    await handleSitemaps(queue, sitemaps, crawlOptions);
+
+    // start crawling
+    queue.start();
+
+    await new Promise((resolve, reject) => {
+      queue.on('idle', () => {
+        resolve();
+      });
+    });
+
+
+  } catch (e) {
+    terminal.red(`\n${e.message}\n`);
+    throw new Error(`crawler failed: ${e.message}`);
+  } finally {
+    urlsFileStream.close((e) => {
+      if (e) {
+        terminal.red('error closing file', e);
+      }
+    });
+  }
+};
