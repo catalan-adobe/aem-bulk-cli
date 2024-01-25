d307b6d645fff4dc18249dd4b404e4c84f640e09
David Catalan <catalan@adobe.com>
2024-01-25 11:57:11 +0100

    fix: merge conflicts

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
-
 const yargs = require('yargs');
 
 (async function main() {
   await yargs(process.argv.slice(2))
     .commandDir('cmds')
     .demandCommand()
+    .scriptName('franklin-bulk')
+    .usage('Usage: $0 <command> [options]')
+    // .parserConfiguration({ 'camel-case-expansion': false })
+    .showHelpOnFail(true)
     .help('h')
     .wrap(yargs.terminalWidth())
     .argv;
